67654ff6eb35d6571be9acd778258acfe1d876c8
Shiris Kumar <shiriskumar@users.noreply.github.com>
2015-06-11 22:07:32 +0530

    Update InfinitePing.bat

--- InfinitePing.bat
+++ InfinitePing.bat
@@ -1,5 +1,8 @@
 @echo off
-PING www.google.com -t
-REM You can also use other IP Address/Domain Name in the above line, instead of www.google.com
-REM Run it with Administrative Privileges
+:ping
+PING -n 1 www.google.com | FIND "TTL="
+REM you can also use some other IP address, instead of www.google.com
+IF ERRORLEVEL 1 (SET OUT=4F  & echo Request timed out.) ELSE (SET OUT=2F)
+color %OUT%
+GoTo ping
 pause
